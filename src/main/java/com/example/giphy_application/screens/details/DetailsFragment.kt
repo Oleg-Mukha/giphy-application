71884bec14a588d1d5d4253d42731dc851d87304
Oleg Mukha <oleg.mukha.2002@gmail.com>
2023-10-10 17:27:29 +0300

    DetailsFragment added.

---
+++ src/main/java/com/example/giphy_application/screens/details/DetailsFragment.kt
@@ -0,0 +1,32 @@
+package com.example.giphy_application.screens.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.example.giphy_application.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+    private val args by navArgs<DetailsFragmentArgs>()
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            Glide.with(this@DetailsFragment)
+                .load(args.url)
+                .into(ivFullScreenGif)
+        }
+    }
+}
