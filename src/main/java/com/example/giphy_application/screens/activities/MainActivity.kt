9e996aa4128b1ba39b7de57ac138843fa1da2ebf
Oleg Mukha <oleg.mukha.2002@gmail.com>
2023-10-10 17:29:21 +0300

    MainActivity updated. Navigation added.

---
+++ src/main/java/com/example/giphy_application/screens/activities/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.giphy_application.screens.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.giphy_application.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(binding.navHostFragment.id)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
